7eb04b020fbab840bf27af5513bfef7c3a718a93
Lisa Hampel <lisa_hampel@t-online.de>
2018-07-26 15:32:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lisa/wg_app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lisa.wg_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void foo() {
+        int i = 1 + 3;
+    }
+}
